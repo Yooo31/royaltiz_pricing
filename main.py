d611ee0e7def971e98e7afb503f4067156d7893a
Yooo31 <mrts.yoan@gmail.com>
2022-09-07 22:06:54 +0200

    Add cookies pop up validation

--- main.py
+++ main.py
@@ -12,3 +12,9 @@ def open_player_view(player=""):
 
   driver.get("https://www.royaltiz.com/" + player) # Go to the url of the website
   time.sleep(20)
+
+def accept_cookies():
+  print('start cookies')
+
+  cookie_button = driver.find_element(By.CLASS_NAME, 'cookie-banner-accept-button') # Find the button to accept cookies
+  cookie_button.click()
