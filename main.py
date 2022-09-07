1647f785a2a7b03eda12b861a8674a659a1645a7
Yooo31 <mrts.yoan@gmail.com>
2022-09-07 19:08:59 +0200

    Function to open website

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import time
+
+player_list = ['DUPONT', 'HOUNKPATIN', 'TANGA', 'LANDU']
+global all_player_price
+all_player_price = []
+
+def open_player_view(player=""):
+  print('Opening the ' + player + ' player view')
+
+  driver.get("https://www.royaltiz.com/" + player) # Go to the url of the website
+  time.sleep(20)
